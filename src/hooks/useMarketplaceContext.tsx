11055781c3544bbd23035e673d25654427431d44
myreceiptt <nota@straight-line.org>
2024-12-06 06:03:45 +0700

    tokenId

--- src/hooks/useMarketplaceContext.tsx
+++ src/hooks/useMarketplaceContext.tsx
@@ -158,6 +158,10 @@ export default function MarketplaceProvider({
     getSupplyInfo,
     {
       contract,
+      tokenId: 0n,
+      queryOptions: {
+        enabled: !!contract,
+      },
     }
   );
 
