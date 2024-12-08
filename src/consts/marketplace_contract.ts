76e48d74b4d2af0b914b0b6a7dcb85c8d0d0ab34
myreceiptt <nota@straight-line.org>
2024-12-08 08:16:04 +0700

    polygon ready

--- src/consts/marketplace_contract.ts
+++ src/consts/marketplace_contract.ts
@@ -11,20 +11,36 @@ type MarketplaceContract = {
  * Only list one marketplace contract address for each chain.
  */
 export const MARKETPLACE_CONTRACTS: MarketplaceContract[] = [
+  // {
+  //   address: "0x...", // ENDHONESA DIGITAL
+  //   chain: ethereum,
+  // },
+  // {
+  //   address: "0xC979B5AA97C513d8fCd48DDbe908C998455530d8", // ENDHONESA DIGITAL
+  //   chain: polygon,
+  // },
+  // {
+  //   address: "0x...", // ENDHONESA DIGITAL
+  //   chain: base,
+  // },
+  // {
+  //   address: "0x...", // ENDHONESA DIGITAL
+  //   chain: baseSepolia,
+  // },
   {
-    address: "0xC0b5e3111B8f09D3242C792C59e13857b62B0C78", // ENDHONESA DIGITAL
+    address: "0x292111D4B26878F67Bae7941C5d0BD7bB272D5A1", // MEMORA MARKETPLACE
     chain: ethereum,
   },
   {
-    address: "0xC979B5AA97C513d8fCd48DDbe908C998455530d8", // ENDHONESA DIGITAL
+    address: "0x292111D4B26878F67Bae7941C5d0BD7bB272D5A1", // MEMORA MARKETPLACE
     chain: polygon,
   },
   {
-    address: "0xc12c7E270685211A2ddcaD741e9378882762CdB1", // MEMORA TRIAL
+    address: "0xc12c7E270685211A2ddcaD741e9378882762CdB1", // MEMORA MARKETPLACE
     chain: base,
   },
   {
-    address: "0xC0b5e3111B8f09D3242C792C59e13857b62B0C78", // NFT MARKETPLACE TRIAL
+    address: "0xC0b5e3111B8f09D3242C792C59e13857b62B0C78", // MEM0RA MARKETPLACE
     chain: baseSepolia,
   },
 ];
